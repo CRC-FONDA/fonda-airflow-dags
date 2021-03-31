cf74b38c0dfc09c97a315c8ff14f906118b31396
Soeren Becker <soeren.becker@tu-berlin.de>
2021-03-31 11:13:13 +0200

    Update dev environment

--- kind/start-dev-env.sh
+++ kind/start-dev-env.sh
@@ -6,6 +6,10 @@ AIRFLOW_REPO_PATH=$(pwd)
 sed -e "s|REPO_PATH|$AIRFLOW_REPO_PATH|g" kind/kind-cluster-conf.yaml | kind create cluster --name airflow --config -
 
 # clone airflow helm chart
-# run helm dependency update airflow/
+mkdir -p /tmp/fonda-infra
+git clone https://github.com/CRC-FONDA/s1-fonda-infrastructure /tmp/fonda-infra
 
-#helm install airflow airflow-stable/airflow --values kind/airflow-helm-values.yaml
+helm install airflow /tmp/fonda-infra/charts/airflow --values kind/airflow-helm-values.yaml
+
+echo "Wait for all Pods to be running and start the ./kind/create-admin-user.sh script"
+echo "Check with 'kubectl get pods'"
