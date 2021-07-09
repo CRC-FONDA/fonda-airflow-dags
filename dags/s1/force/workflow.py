b81c51b7ba3c310275191f33267d76a6dd00c0ae
Vasilis <bountris.vasilis@gmail.com>
2021-07-09 14:19:48 +0300

    Modify compute resources requests and limits

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -33,10 +33,10 @@ mask_resolution = 30
 namespace = "default"
 
 compute_resources = {
-    'request_cpu': '200m',
+    'request_cpu': '1000m',
     'request_memory': '512Mi',
-    'limit_cpu': '500m',
-    'limit_memory': '1Gi'
+    'limit_cpu': '2000m',
+    'limit_memory': '5Gi'
 }
 
 volume = k8s.V1Volume(
