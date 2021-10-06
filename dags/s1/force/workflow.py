a230b228654a96a3ddfa195893c573bc4a215d59
Vasilis <bountris.vasilis@gmail.com>
2021-10-06 14:09:39 +0200

    Change download long of auxillary data

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -113,7 +113,7 @@ with DAG(
         image="bash",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            f"wget -O auxiliary.tar.gz https://box.hu-berlin.de/f/eb61444bd97f4c738038/?dl=1 && tar -xzf auxiliary.tar.gz && cp -r EO-01/input {MOUNT_DATA_PATH}"
+            f"wget -O auxiliary.tar.gz https://box.hu-berlin.de/f/d01a794f717144a1a648/?dl=1 && tar -xzf auxiliary.tar.gz && cp -r EO-01/input {MOUNT_DATA_PATH}"
         ],
         resources=compute_resources,
         volumes=[volume],
