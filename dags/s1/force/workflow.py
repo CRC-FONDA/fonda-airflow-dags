e7fe328b3e984dd7ec157052c3d6c52bb9c7d91b
vasilis <bountri.vasilis@gmail.com>
2022-02-04 17:10:36 +0100

    Increase memory limit and requirement by 0.5Gbi

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -50,9 +50,9 @@ namespace = "default"
 
 compute_resources = {
     "request_cpu": "1000m",
-    "request_memory": "2Gi",
+    "request_memory": "2.5Gi",
     "limit_cpu": "1000m",
-    "limit_memory": "2Gi",
+    "limit_memory": "2.5Gi",
 }
 
 dataset_volume = k8s.V1Volume(
