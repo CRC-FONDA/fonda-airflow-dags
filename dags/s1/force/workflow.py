9f4eedb26d9eeceb49fb87588be3f9720d64d079
vasilis <bountri.vasilis@gmail.com>
2022-02-18 15:25:23 +0100

    Adjustment of log folderpaths

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -241,9 +241,9 @@ with DAG(
             # sed -i "/^PARALLEL_READS /cPARALLEL_READS = TRUE" $PARAM
             # sed -i "/^DELAY /cDELAY = 2" $PARAM
             sed -i "/^NPROC /cNPROC = 1" $PARAM
-            sed -i "/^DIR_LEVEL2 /cDIR_LEVEL2 = /data/outputs/level2_ard/" $PARAM
-            sed -i "/^DIR_LOG /cDIR_LOG = /data/outputs/level2_log/" $PARAM
-            sed -i "/^DIR_TEMP /cDIR_TEMP = /data/outputs/level2_tmp/" $PARAM
+            sed -i "/^DIR_LEVEL2 /cDIR_LEVEL2 = /data/outputs/level2_ard" $PARAM
+            sed -i "/^DIR_LOG /cDIR_LOG = /data/outputs/level2_log" $PARAM
+            sed -i "/^DIR_TEMP /cDIR_TEMP = /data/outputs/level2_tmp" $PARAM
             sed -i "/^FILE_DEM /cFILE_DEM = $DEM/crete_srtm-aster.vrt" $PARAM
             sed -i "/^DIR_WVPLUT /cDIR_WVPLUT = $WVDB" $PARAM
             sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
