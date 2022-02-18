eba5977f53561cc1d1b741c496d81860cf7e2aae
vasilis <bountri.vasilis@gmail.com>
2022-02-18 13:53:17 +0100

    Move f variables to env variables, clear bash scripts styling

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -41,7 +41,7 @@ mask_resolution = 30
 num_of_tiles = 28
 # Parallel factor is how many images are to be processed,
 # the maximum amount of parallelization that's possible
-parallel_factor = 2794  
+parallel_factor = 2794
 num_of_filters = 10
 # One has to assert that the number of pyramids tasks per tile is
 # smaller than the number of the actual filters
@@ -51,16 +51,16 @@ num_of_pyramid_tasks_per_tile = 10
 namespace = "default"
 
 compute_resources = {
-    "request_cpu": "2000m",
+    "request_cpu": "2",
     "request_memory": "4.5Gi",
-    "limit_cpu": "2000m",
+    "limit_cpu": "2",
     "limit_memory": "4.5Gi",
 }
 
 preprocess_resources = {
-    "request_cpu": "2000m",
+    "request_cpu": "2",
     "request_memory": "4.5Gi",
-    "limit_cpu": "2000m",
+    "limit_cpu": "2",
     "limit_memory": "4.5Gi",
 }
 
@@ -72,19 +72,18 @@ pyramid_resources = {
 }
 
 tsa_resources = {
-    "request_cpu": "2000m",
+    "request_cpu": "2",
     "request_memory": "4.5Gi",
-    "limit_cpu": "2000m",
+    "limit_cpu": "2",
     "limit_memory": "4.5Gi",
 }
 
 mosaic_resources = {
-    "request_cpu": "2000m",
+    "request_cpu": "2",
     "request_memory": "4.5Gi",
-    "limit_cpu": "2000m",
+    "limit_cpu": "2",
     "limit_memory": "4.5Gi",
 }
-
 
 
 dataset_volume = k8s.V1Volume(
@@ -173,12 +172,17 @@ with DAG(
         task_id="generate_allowed_tiles",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            f"force-tile-extent {aoi_filepath} {datacube_folderpath} {allowed_tiles_filepath}"
+            "force-tile-extent $AOI_FILEPATH $DATACUBE_FOLDERPATH $ALLOWED_TILES_FILEPATH"
         ],
         security_context=security_context,
         resources=compute_resources,
         volumes=[dataset_volume, outputs_volume],
         volume_mounts=[dataset_volume_mount, outputs_volume_mount],
+        env_vars={
+            "AOI_FILEPATH": aoi_filepath,
+            "DATACUBE_FOLDERPATH": datacube_folderpath,
+            "ALLOWED_TILES_FILEPATH": allowed_tiles_filepath,
+        },
         get_logs=True,
         affinity=experiment_affinity,
         dag=dag,
@@ -192,12 +196,18 @@ with DAG(
         task_id="generate_analysis_mask",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            f"mkdir -p {masks_folderpath} && cp {datacube_folderpath}/datacube-definition.prj {masks_folderpath} && force-cube {aoi_filepath} {masks_folderpath} rasterize {mask_resolution}"
+            "mkdir -p $MASKS_FOLDERPATH && cp $DATACUBE_FOLDERPATH/datacube-definition.prj $MASKS_FOLDERPATH && force-cube $AOI_FILEPATH $MASKS_FOLDERPATH rasterize $MASK_RESOLUTION"
         ],
         security_context=security_context,
         resources=compute_resources,
         volumes=[dataset_volume, outputs_volume],
         volume_mounts=[dataset_volume_mount, outputs_volume_mount],
+        env_vars={
+            "MASKS_FOLDERPATH": masks_folderpath,
+            "DATACUBE_FOLDERPATH": datacube_folderpath,
+            "AOI_FILEPATH": aoi_filepath,
+            "MASK_RESOLUTION": mask_resolution,
+        },
         get_logs=True,
         affinity=experiment_affinity,
         dag=dag,
@@ -211,50 +221,52 @@ with DAG(
         task_id="prepare_level2",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            f"""
-        # wget -O {queue_filepath} https://box.hu-berlin.de/f/8cbd80805d484be1b91a/?dl=1
-        mkdir -p /data/outputs/queue_files
-        split -a 4 -l$((`wc -l < {queue_filepath}`/{parallel_factor})) --numeric-suffixes=0 {queue_filepath} /data/outputs/queue_files/queue_ --additional-suffix=.txt
-        mkdir -p /data/outputs/param_files
-        mkdir -p /data/outputs/level2_ard
-        mkdir -p /data/outputs/level2_log
-        mkdir -p /data/outputs/level2_tmp
-        force-parameter . LEVEL2 0
-        mv LEVEL2-skeleton.prm $PARAM
-        # read grid definition
-        CRS=$(sed '1q;d' $CUBEFILE)
-        ORIGINX=$(sed '2q;d' $CUBEFILE)
-        ORIGINY=$(sed '3q;d' $CUBEFILE)
-        TILESIZE=$(sed '6q;d' $CUBEFILE)
-        BLOCKSIZE=$(sed '7q;d' $CUBEFILE)
-        # set parameters
-        # sed -i "/^PARALLEL_READS /cPARALLEL_READS = TRUE" $PARAM
-        # sed -i "/^DELAY /cDELAY = 2" $PARAM
-        sed -i "/^NPROC /cNPROC = 1" $PARAM
-        sed -i "/^DIR_LEVEL2 /cDIR_LEVEL2 = /data/outputs/level2_ard/" $PARAM
-        sed -i "/^DIR_LOG /cDIR_LOG = /data/outputs/level2_log/" $PARAM
-        sed -i "/^DIR_TEMP /cDIR_TEMP = /data/outputs/level2_tmp/" $PARAM
-        sed -i "/^FILE_DEM /cFILE_DEM = $DEM/crete_srtm-aster.vrt" $PARAM
-        sed -i "/^DIR_WVPLUT /cDIR_WVPLUT = $WVDB" $PARAM
-        sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
-        sed -i "/^TILE_SIZE /cTILE_SIZE = $TILESIZE" $PARAM
-        sed -i "/^BLOCK_SIZE /cBLOCK_SIZE = $BLOCKSIZE" $PARAM
-        sed -i "/^ORIGIN_LON /cORIGIN_LON = $ORIGINX" $PARAM
-        sed -i "/^ORIGIN_LAT /cORIGIN_LAT = $ORIGINY" $PARAM
-        sed -i "/^PROJECTION /cPROJECTION = $CRS" $PARAM
-        sed -i "/^NTHREAD /cNTHREAD = 4" $PARAM
+            """
+            # wget -O $QUEUE_FILEPATH https://box.hu-berlin.de/f/8cbd80805d484be1b91a/?dl=1
+            mkdir -p /data/outputs/queue_files
+            split -a 4 -l$((`wc -l < $QUEUE_FILEPATH`/$PARALLEL_FACTOR)) --numeric-suffixes=0 $QUEUE_FILEPATH /data/outputs/queue_files/queue_ --additional-suffix=.txt
+            mkdir -p /data/outputs/param_files
+            mkdir -p /data/outputs/level2_ard
+            mkdir -p /data/outputs/level2_log
+            mkdir -p /data/outputs/level2_tmp
+            force-parameter . LEVEL2 0
+            mv LEVEL2-skeleton.prm $PARAM
+            # read grid definition
+            CRS=$(sed '1q;d' $CUBEFILE)
+            ORIGINX=$(sed '2q;d' $CUBEFILE)
+            ORIGINY=$(sed '3q;d' $CUBEFILE)
+            TILESIZE=$(sed '6q;d' $CUBEFILE)
+            BLOCKSIZE=$(sed '7q;d' $CUBEFILE)
+            # set parameters
+            # sed -i "/^PARALLEL_READS /cPARALLEL_READS = TRUE" $PARAM
+            # sed -i "/^DELAY /cDELAY = 2" $PARAM
+            sed -i "/^NPROC /cNPROC = 1" $PARAM
+            sed -i "/^DIR_LEVEL2 /cDIR_LEVEL2 = /data/outputs/level2_ard/" $PARAM
+            sed -i "/^DIR_LOG /cDIR_LOG = /data/outputs/level2_log/" $PARAM
+            sed -i "/^DIR_TEMP /cDIR_TEMP = /data/outputs/level2_tmp/" $PARAM
+            sed -i "/^FILE_DEM /cFILE_DEM = $DEM/crete_srtm-aster.vrt" $PARAM
+            sed -i "/^DIR_WVPLUT /cDIR_WVPLUT = $WVDB" $PARAM
+            sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
+            sed -i "/^TILE_SIZE /cTILE_SIZE = $TILESIZE" $PARAM
+            sed -i "/^BLOCK_SIZE /cBLOCK_SIZE = $BLOCKSIZE" $PARAM
+            sed -i "/^ORIGIN_LON /cORIGIN_LON = $ORIGINX" $PARAM
+            sed -i "/^ORIGIN_LAT /cORIGIN_LAT = $ORIGINY" $PARAM
+            sed -i "/^PROJECTION /cPROJECTION = $CRS" $PARAM
+            sed -i "/^NTHREAD /cNTHREAD = $NTHREAD" $PARAM
             """
         ],
         security_context=security_context,
-        resources=compute_resources,
+        resources=preprocess_resources,
         volumes=[dataset_volume, outputs_volume],
         volume_mounts=[dataset_volume_mount, outputs_volume_mount],
         env_vars={
+            "QUEUE_FILEPATH": queue_filepath,
+            "PARALLEL_FACTOR": parallel_factor,
             "CUBEFILE": datacube_filepath,
             "DEM": dem_folderpath,
             "WVDB": wvdb,
             "TILE": allowed_tiles_filepath,
-            "NTHREAD": "4",
+            "NTHREAD": str(int(preprocess_resources["request_cpu"]) * 2),
             "PARAM": "/data/outputs/param_files/ard.prm",
         },
         get_logs=True,
@@ -273,14 +285,14 @@ with DAG(
             task_id="preprocess_level2_" + index,
             cmds=["/bin/sh", "-c"],
             arguments=[
-                """\
-            cp $GLOBAL_PARAM $PARAM
-            sed -i "/^FILE_QUEUE /cFILE_QUEUE = $QUEUE_FILE" $PARAM
-            force-level2 $PARAM
-            """
+                """
+                cp $GLOBAL_PARAM $PARAM
+                sed -i "/^FILE_QUEUE /cFILE_QUEUE = $QUEUE_FILE" $PARAM
+                force-level2 $PARAM
+                """
             ],
             security_context=security_context,
-            resources=compute_resources,
+            resources=preprocess_resources,
             volumes=[dataset_volume, outputs_volume],
             volume_mounts=[dataset_volume_mount, outputs_volume_mount],
             env_vars={
@@ -304,48 +316,48 @@ with DAG(
         task_id="prepare_tsa",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            """\
-        force-parameter . TSA 0
-        mv TSA-skeleton.prm $PARAM
-        mkdir -p $TRENDS_FOLDER
+            """
+            force-parameter . TSA 0
+            mv TSA-skeleton.prm $PARAM
+            mkdir -p $TRENDS_FOLDER
 
-        # paths
-        sed -i "/^DIR_LOWER /cDIR_LOWER = $ARD_FOLDER" $PARAM
-        sed -i "/^DIR_HIGHER /cDIR_HIGHER = $TRENDS_FOLDER" $PARAM
-        sed -i "/^DIR_MASK /cDIR_MASK = $MASKS_FOLDER" $PARAM
-        sed -i "/^FILE_ENDMEM /cFILE_ENDMEM = $ENDMEMBER" $PARAM
-        sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
+            # paths
+            sed -i "/^DIR_LOWER /cDIR_LOWER = $ARD_FOLDER" $PARAM
+            sed -i "/^DIR_HIGHER /cDIR_HIGHER = $TRENDS_FOLDER" $PARAM
+            sed -i "/^DIR_MASK /cDIR_MASK = $MASKS_FOLDER" $PARAM
+            sed -i "/^FILE_ENDMEM /cFILE_ENDMEM = $ENDMEMBER" $PARAM
+            sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
 
-        # threading
-        sed -i "/^NTHREAD_READ /cNTHREAD_READ = 1" $PARAM
-        sed -i "/^NTHREAD_COMPUTE /cNTHREAD_COMPUTE = 2" $PARAM
-        sed -i "/^NTHREAD_WRITE /cNTHREAD_WRITE = 1" $PARAM
+            # threading
+            sed -i "/^NTHREAD_READ /cNTHREAD_READ = 2" $PARAM
+            sed -i "/^NTHREAD_COMPUTE /cNTHREAD_COMPUTE = $NTHREAD" $PARAM
+            sed -i "/^NTHREAD_WRITE /cNTHREAD_WRITE = 2" $PARAM
 
-        # resolution
-        sed -i "/^RESOLUTION /cRESOLUTION = 30" $PARAM
+            # resolution
+            sed -i "/^RESOLUTION /cRESOLUTION = $MASK_RESOLUTION" $PARAM
 
-        # sensors
-        sed -i "/^SENSORS /cSENSORS = LND04 LND05 LND07" $PARAM
+            # sensors
+            sed -i "/^SENSORS /cSENSORS = LND04 LND05 LND07" $PARAM
 
-        # date range
-        sed -i "/^DATE_RANGE /cDATE_RANGE = $START_DATE $END_DATE" $PARAM
+            # date range
+            sed -i "/^DATE_RANGE /cDATE_RANGE = $START_DATE $END_DATE" $PARAM
 
-        # spectral index
-        sed -i "/^INDEX /cINDEX = SMA" $PARAM
+            # spectral index
+            sed -i "/^INDEX /cINDEX = SMA" $PARAM
 
-        # interpolation
-        sed -i "/^INT_DAY /cINT_DAY = 8" $PARAM
-        sed -i "/^OUTPUT_TSI /cOUTPUT_TSI = TRUE" $PARAM
+            # interpolation
+            sed -i "/^INT_DAY /cINT_DAY = 8" $PARAM
+            sed -i "/^OUTPUT_TSI /cOUTPUT_TSI = TRUE" $PARAM
 
-        # polar metrics
-        sed -i "/^POL /cPOL = VPS VBL VSA" $PARAM
-        sed -i "/^OUTPUT_POL /cOUTPUT_POL = TRUE" $PARAM
-        sed -i "/^OUTPUT_TRO /cOUTPUT_TRO = TRUE" $PARAM
-        sed -i "/^OUTPUT_CAO /cOUTPUT_CAO = TRUE" $PARAM
+            # polar metrics
+            sed -i "/^POL /cPOL = VPS VBL VSA" $PARAM
+            sed -i "/^OUTPUT_POL /cOUTPUT_POL = TRUE" $PARAM
+            sed -i "/^OUTPUT_TRO /cOUTPUT_TRO = TRUE" $PARAM
+            sed -i "/^OUTPUT_CAO /cOUTPUT_CAO = TRUE" $PARAM
 
-        cp $PARAM /data/outputs/param_files/
+            cp $PARAM /data/outputs/param_files/
 
-        echo "DONE"
+            echo "DONE"
             """
         ],
         security_context=security_context,
@@ -353,19 +365,16 @@ with DAG(
         volumes=[dataset_volume, outputs_volume],
         volume_mounts=[dataset_volume_mount, outputs_volume_mount],
         env_vars={
-            "CUBEFILE": datacube_filepath,
-            "DEM": dem_folderpath,
-            "WVDB": wvdb,
             "TILE": allowed_tiles_filepath,
-            "NTHREAD": "2",
+            "NTHREAD": str(int(preprocess_resources["request_cpu"]) * 2),
             "PARAM": "tsa.prm",
             "ENDMEMBER": endmember_filepath,
             "ARD_FOLDER": ard_folderpath,
             "TRENDS_FOLDER": trends_folderpath,
             "MASKS_FOLDER": masks_folderpath,
-            "AOI_PATH": aoi_filepath,
             "START_DATE": start_date.isoformat(),
             "END_DATE": end_date.isoformat(),
+            "MASK_RESOLUTION": mask_resolution,
         },
         get_logs=True,
         affinity=experiment_affinity,
@@ -383,32 +392,31 @@ with DAG(
             task_id="tsa_task_" + index,
             cmds=["/bin/bash", "-c"],
             arguments=[
-                f"""\
-              echo "STARTING TIME SERIES ANALYSIS"
-              cp $GLOBAL_PARAM $PARAM
-              # Get the corresponding line from the allowed tiles file
-              TILE=`sed '{index}q;d' $TILE_FILE`
-              X=${{TILE:1:4}}
-              Y=${{TILE:7:11}}
-              sed -i "/^BASE_MASK /cBASE_MASK = aoi.tif" $PARAM
-              sed -i "/^X_TILE_RANGE /cX_TILE_RANGE = $X $X" $PARAM
-              sed -i "/^Y_TILE_RANGE /cY_TILE_RANGE = $Y $Y" $PARAM
-              force-higher-level $PARAM
-              echo "DONE"
+                f"""
+                echo "STARTING TIME SERIES ANALYSIS"
+                cp $GLOBAL_PARAM $PARAM
+                # Get the corresponding line from the allowed tiles file
+                TILE=`sed '{index}q;d' $TILE_FILE`
+                X=${{TILE:1:4}}
+                Y=${{TILE:7:11}}
+                sed -i "/^BASE_MASK /cBASE_MASK = aoi.tif" $PARAM
+                sed -i "/^X_TILE_RANGE /cX_TILE_RANGE = $X $X" $PARAM
+                sed -i "/^Y_TILE_RANGE /cY_TILE_RANGE = $Y $Y" $PARAM
+                force-higher-level $PARAM
+                echo "DONE"
 
-              # Push results to xcom
-              mkdir -p /airflow/xcom/
+                # Push results to xcom
+                mkdir -p /airflow/xcom/
 
-              # Find *.tif files and store them in a list of files
-              cd /data/outputs/trends/$TILE
-              files=`find *.tif | tr '\n' ','`
-              # Add Brackets
-              files='['$files']'
-              # Make json
-              echo '{{"tile":"'$TILE'", "files":"'$files'"}}'
-              echo '{{"tile":"'$TILE'", "files":"'$files'"}}' > /airflow/xcom/return.json
-
-                  """
+                # Find *.tif files and store them in a list of files
+                cd /data/outputs/trends/$TILE
+                files=`find *.tif | tr '\n' ','`
+                # Add Brackets
+                files='['$files']'
+                # Make json
+                echo '{{"tile":"'$TILE'", "files":"'$files'"}}'
+                echo '{{"tile":"'$TILE'", "files":"'$files'"}}' > /airflow/xcom/return.json
+                """
             ],
             security_context=security_context,
             resources=tsa_resources,
@@ -444,13 +452,13 @@ with DAG(
                 task_id="pyramid_task_" + index,
                 cmds=["/bin/bash", "-c"],
                 arguments=[
-                    f"""\
-                            TILE=\"{{{{ task_instance.xcom_pull('tsa_task_{tile_index}')[\"tile\"] }}}}\"
-                            FILES=\"{{{{ task_instance.xcom_pull('tsa_task_{tile_index}')[\"files\"] }}}}\"
-                            CHOSEN_FILE=`echo $FILES | sed 's/[][]//g' | cut -d "," -f $FILE_INDEX`
-                            FILES_TO_DO="${{TRENDS_FOLDERPATH}}/${{TILE}}/${{CHOSEN_FILE}}"
-                            force-pyramid $FILES_TO_DO
-                  """
+                    f"""
+                    TILE=\"{{{{ task_instance.xcom_pull('tsa_task_{tile_index}')[\"tile\"] }}}}\"
+                    FILES=\"{{{{ task_instance.xcom_pull('tsa_task_{tile_index}')[\"files\"] }}}}\"
+                    CHOSEN_FILE=`echo $FILES | sed 's/[][]//g' | cut -d "," -f $FILE_INDEX`
+                    FILES_TO_DO="${{TRENDS_FOLDERPATH}}/${{TILE}}/${{CHOSEN_FILE}}"
+                    force-pyramid $FILES_TO_DO
+                    """
                 ],
                 security_context=security_context,
                 resources=pyramid_resources,
@@ -476,7 +484,7 @@ with DAG(
         task_id="wait_for_trends",
         cmds=["/bin/bash", "-c"],
         arguments=[
-            """\
+            """
             cd $TRENDS_FOLDERPATH
             UNIQUE_BASENAMES=`find . -name '*.tif' -exec basename {} \; | sort | uniq`
             COUNTER=0
@@ -520,9 +528,9 @@ with DAG(
             task_id="mosaic_task_" + index,
             cmds=["/bin/bash", "-c"],
             arguments=[
-                f"""\
-                      force-mosaic $MOSAIC_FOLDERPATH/$INDEX
-                  """
+                f"""
+                force-mosaic $MOSAIC_FOLDERPATH/$INDEX
+                """
             ],
             security_context=security_context,
             resources=compute_resources,
@@ -546,19 +554,19 @@ with DAG(
         task_id="check_results",
         cmds=["/bin/sh", "-c"],
         arguments=[
-            """\
-        mkdir -p $TRENDS_FOLDERPATH/mosaic
-        cp $MOSAIC_FOLDERPATH/0/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_TSI.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/1/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/2/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-POL.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/3/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/4/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/5/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-POL.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/6/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/7/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/8/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-POL.vrt $TRENDS_FOLDERPATH/mosaic/
-        cp $MOSAIC_FOLDERPATH/9/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
-        Rscript $TESTS_FOLDERPATH/test.R $TRENDS_FOLDERPATH/mosaic $TESTS_FOLDERPATH/reference.RData log.log
+            """
+            mkdir -p $TRENDS_FOLDERPATH/mosaic
+            cp $MOSAIC_FOLDERPATH/0/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_TSI.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/1/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/2/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-POL.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/3/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VBL-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/4/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/5/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-POL.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/6/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VPS-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/7/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-CAO.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/8/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-POL.vrt $TRENDS_FOLDERPATH/mosaic/
+            cp $MOSAIC_FOLDERPATH/9/mosaic/1984-2006_001-365_HL_TSA_LNDLG_SMA_VSA-TRO.vrt $TRENDS_FOLDERPATH/mosaic/
+            Rscript $TESTS_FOLDERPATH/test.R $TRENDS_FOLDERPATH/mosaic $TESTS_FOLDERPATH/reference.RData log.log
             """
         ],
         security_context=security_context,
