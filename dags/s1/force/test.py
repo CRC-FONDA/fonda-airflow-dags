8a47759e520be03beabe9ced987c24beeb1c37a8
Soeren Becker <soeren.becker@tu-berlin.de>
2022-02-22 13:57:48 +0100

    Add a failing mock test (#11)
    * Run a mock test
    
    * Move test stage into steps
    
    * Add jenkins pod manifest
    
    * Always store junit results
    
    * Fix failing test

--- dags/s1/force/test.py
+++ dags/s1/force/test.py
@@ -4,7 +4,7 @@ class TestingSum(unittest.TestCase):
     def test_sum(self):  
         self.assertEqual(sum([2, 3, 5]), 10, "It should be 10")  
     def test_sum_tuple(self):  
-        self.assertEqual(sum((1, 3, 5)), 10, "It should be 10")  
+        self.assertEqual(sum((1, 4, 5)), 10, "It should be 10")  
   
 if __name__ == '__main__':  
     unittest.main()  
