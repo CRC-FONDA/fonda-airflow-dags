454ba9b5dca4329299374bbf864e35f77ddcfe0f
vasilis <bountri.vasilis@gmail.com>
2022-04-26 14:15:20 +0200

    Correct dem file name

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -206,7 +206,7 @@ with DAG(
         sed -i "/^DIR_LEVEL2 /cDIR_LEVEL2 = /data/outputs/level2_ard/" $PARAM
         sed -i "/^DIR_LOG /cDIR_LOG = /data/outputs/level2_log/" $PARAM
         sed -i "/^DIR_TEMP /cDIR_TEMP = /data/outputs/level2_tmp/" $PARAM
-        sed -i "/^FILE_DEM /cFILE_DEM = $DEM/crete_srtm-aster.vrt" $PARAM
+        sed -i "/^FILE_DEM /cFILE_DEM = $DEM/crete_srt-master.vrt" $PARAM
         sed -i "/^DIR_WVPLUT /cDIR_WVPLUT = $WVDB" $PARAM
         sed -i "/^FILE_TILE /cFILE_TILE = $TILE" $PARAM
         sed -i "/^TILE_SIZE /cTILE_SIZE = $TILESIZE" $PARAM
