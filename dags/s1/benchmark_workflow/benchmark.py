6d238db1a4b1ea2c2a1f99514e0b3419ab9b625d
bountrisv <vasilis.bountris@hu-berlin.de>
2025-05-13 15:41:56 +0200

    Change pod operator path in import declaration

--- dags/s1/benchmark_workflow/benchmark.py
+++ dags/s1/benchmark_workflow/benchmark.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 import random
 import time
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 from kubernetes.client import models as k8s
 
