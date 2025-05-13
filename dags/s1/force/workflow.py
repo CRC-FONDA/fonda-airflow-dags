d68f01c3e2c145aade3e898f98f010600a539d1a
bountrisv <vasilis.bountris@hu-berlin.de>
2025-05-13 15:56:57 +0200

    Correct import path for Kubernetes Pod on more workflows

--- dags/s1/force/workflow.py
+++ dags/s1/force/workflow.py
@@ -3,9 +3,8 @@ from random import shuffle
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
+from airflow.providers.cncf.kubernetes.operators.pod import \
+    KubernetesPodOperator
 from airflow.utils.dates import days_ago
 from kubernetes.client import models as k8s
 
